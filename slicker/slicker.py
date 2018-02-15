e33cb67016c7fcc8a0aa3a50fdd8acec70e8199e
Ben Kraft <benkraft@khanacademy.org>
2018-02-14 17:10:36 -0800

    Move cleanup suggestors to a separate file
    These are the ones that clean up whitespace, resort imports, and the
    like.  But the real difference is that they have very little to do with
    the rest of slicker, so they're very easy to separate out.  This is the
    first of a series of commits to split up slicker.py (#6).  I did the
    moves with slicker, of course, although I had to fix up a few things
    manually since it doesn't like relative imports.
    
    I also added a shortcut `make check` for `make test lint`.
    
    Test plan: `make check`
    
    Reviewers: csilvers

--- slicker/slicker.py
+++ slicker/slicker.py
@@ -73,16 +73,13 @@ from __future__ import absolute_import
 import argparse
 import ast
 import collections
-import difflib
 import itertools
-import os
 import re
 import string
 import sys
 import tokenize
 
-from fix_includes import fix_python_imports
-
+from . import cleanup
 from . import inputs
 from . import khodemod
 from . import moves
@@ -155,13 +152,6 @@ def _dotted_prefixes(string, proper_only=False):
     string_parts = string.split('.')
     for i in xrange(len(string_parts) - (1 if proper_only else 0)):
         yield '.'.join(string_parts[:i + 1])
-
-
-class FakeOptions(object):
-    """A fake `options` object to pass in to fix_python_imports."""
-    def __init__(self, project_root):
-        self.safe_headers = True
-        self.root = project_root
 
 
 class Import(object):
@@ -1473,94 +1463,6 @@ def _remove_moved_region_late_imports_suggestor(project_root, new_fullname):
     return suggestor
 
 
-def _remove_empty_files_suggestor(filename, body):
-    """Suggestor to remove any empty files we leave behind.
-
-    We also remove the file if it has only __future__ imports.  If all that's
-    left is docstrings, comments, and non-__future__ imports, we warn but don't
-    remove it.  (We ignore __init__.py files since those are often
-    intentionally empty or kept only for some imports.)
-    """
-    if os.path.basename(filename) == '__init__.py':
-        # Ignore __init__.py files.
-        return
-
-    file_info = util.File(filename, body)
-
-    has_docstrings_comments_or_imports = '#' in body
-    for stmt in file_info.tree.body:
-        if isinstance(stmt, ast.Expr) and isinstance(stmt.value, ast.Str):
-            # A docstring.
-            has_docstrings_comments_or_imports = True
-        elif isinstance(stmt, ast.ImportFrom) and stmt.module == '__future__':
-            # A __future__ import, which won't force us to keep the file.
-            pass
-        elif isinstance(stmt, (ast.Import, ast.ImportFrom)):
-            # A non-__future__ import.
-            has_docstrings_comments_or_imports = True
-        else:
-            # Some real code; we don't want to do anything.
-            return
-
-    # If we've gotten here, there's no "real code".
-    if has_docstrings_comments_or_imports:
-        yield khodemod.WarningInfo(
-            filename, 0, "This file looks mostly empty; consider removing it.")
-    else:
-        # It's actually empty, so we can just go ahead and remove.
-        yield khodemod.Patch(filename, body, None, 0, len(body))
-
-
-def _remove_leading_whitespace_suggestor(filename, body):
-    """Suggestor to remove any leading whitespace we leave behind."""
-    lstripped_body = body.lstrip()
-    if lstripped_body != body:
-        whitespace_len = len(body) - len(lstripped_body)
-        yield khodemod.Patch(filename, body[:whitespace_len], '',
-                             0, whitespace_len)
-
-
-def _import_sort_suggestor(project_root):
-    """Suggestor to fix up imports in a file."""
-    fix_imports_flags = FakeOptions(project_root)
-
-    def suggestor(filename, body):
-        """`filename` relative to project_root."""
-        # TODO(benkraft): merge this with the import-adding, so we just show
-        # one diff to add in the right place, unless there is additional
-        # sorting to do.
-        # Now call out to fix_python_imports to do the import-sorting
-        change_record = fix_python_imports.ChangeRecord('fake_file.py')
-
-        # A modified version of fix_python_imports.GetFixedFile
-        # NOTE: fix_python_imports needs the rootdir to be on the
-        # path so it can figure out third-party deps correctly.
-        # (That's in addition to having it be in FakeOptions, sigh.)
-        try:
-            sys.path.insert(0, os.path.abspath(project_root))
-            file_line_infos = fix_python_imports.ParseOneFile(
-                body, change_record)
-            fixed_lines = fix_python_imports.FixFileLines(
-                change_record, file_line_infos, fix_imports_flags)
-        finally:
-            del sys.path[0]
-
-        if fixed_lines is None:
-            return
-        fixed_body = ''.join(['%s\n' % line for line in fixed_lines
-                              if line is not None])
-        if fixed_body == body:
-            return
-
-        diffs = difflib.SequenceMatcher(None, body, fixed_body).get_opcodes()
-        for op, i1, i2, j1, j2 in diffs:
-            if op != 'equal':
-                yield khodemod.Patch(filename,
-                                     body[i1:i2], fixed_body[j1:j2], i1, i2)
-
-    return suggestor
-
-
 def make_fixes(old_fullnames, new_fullname, import_alias=None,
                project_root='.', automove=True, verbose=False):
     """Do all the fixing necessary to move old_fullnames to new_fullname.
@@ -1582,8 +1484,8 @@ def make_fixes(old_fullnames, new_fullname, import_alias=None,
     3) Fix references in all other files, including updating their imports
        (_fix_uses_suggestor and _remove_imports_suggestor).
     4) Clean up: remove the module(s) we moved things out of, if it is now
-       empty (_remove_empty_files_suggestor), and resort imports in any file we
-       touched (_import_sort_suggestor).
+       empty (cleanup.remove_empty_files_suggestor), and resort imports in any
+       file we touched (cleanup.import_sort_suggestor).
     """
     def log(msg):
         if verbose:
@@ -1646,12 +1548,13 @@ def make_fixes(old_fullnames, new_fullname, import_alias=None,
         frontend.run_suggestor_on_modified_files(remove_imports_suggestor)
 
     log("===== Cleaning up empty files & whitespace =====")
-    frontend.run_suggestor_on_modified_files(_remove_empty_files_suggestor)
     frontend.run_suggestor_on_modified_files(
-        _remove_leading_whitespace_suggestor)
+        cleanup.remove_empty_files_suggestor)
+    frontend.run_suggestor_on_modified_files(
+        cleanup.remove_leading_whitespace_suggestor)
 
     log("===== Resorting imports =====")
-    import_sort_suggestor = _import_sort_suggestor(project_root)
+    import_sort_suggestor = cleanup.import_sort_suggestor(project_root)
     frontend.run_suggestor_on_modified_files(import_sort_suggestor)
 
     log("===== Move complete! =====")
